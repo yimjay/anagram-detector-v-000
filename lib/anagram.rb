307df3ff42d8d6c5483ddd73997f496156e8024c
Ji Yim <gsooyim@gmail.com>
2018-04-27 21:26:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,7 @@ class Anagram
   end
   
   def match(array)
-    array.collect do |anagram|
-      if anagram.chars.sort.join == @word.chars.sort.join 
-        anagram
-      else
-        array.delete(anagram)
-      end
-    end
+    array.collect {|anagram| anagram.chars.sort.join == @word.chars.sort.join}
   end
   
 end
