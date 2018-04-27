9ab65d0acd9d0c1b65e1be398ca66d7645475fe7
Ji Yim <gsooyim@gmail.com>
2018-04-27 21:16:09 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word, :results
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.collect do |anagram|
+      anagram.sort == @word.sort ? results << anagram : nil
+    end
+    @results
+  end
+  
+end
