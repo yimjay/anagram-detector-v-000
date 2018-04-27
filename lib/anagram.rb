e95d300069f829b9f5e891803617114ec635cacf
Ji Yim <gsooyim@gmail.com>
2018-04-27 21:34:55 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    result = []
+    array.select do |anagram|
+      if anagram.chars.sort.join == @word.chars.sort.join
+        result << anagram
+      end
+    end
+  end
+  
+end
